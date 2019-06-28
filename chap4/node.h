0f142bde49277bae92dff1bd1dbb577d92c587cd
dariogithub1 <dariogithub@gmail.com>
2019-06-28 14:55:43 +0200

    Added some comments

--- chap4/node.h
+++ chap4/node.h
@@ -4,8 +4,12 @@ typedef struct node node;
 typedef struct tree tree;
 typedef struct graph graph;
 
+/*The java structure used in the book for node contains 
+ * an array for children, I emulate that using a pointer 
+ * for allocated structure and size_t for the number of children */
 struct node {
     size_t name;
+    size_t nchildren;
     node *children;
 };
 
