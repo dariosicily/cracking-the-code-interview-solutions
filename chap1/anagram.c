50523f81bd72ded29f298d717ac7bac854646e61
dariogithub1 <dariogithub@gmail.com>
2019-06-30 14:23:22 +0200

    Modified anagram.c

--- chap1/anagram.c
+++ chap1/anagram.c
@@ -1,24 +1,33 @@
 #include <stdio.h>
 #include <string.h>
+#include <ctype.h>
+
+
+/*helper function called in the function are_anagrams*/
+static void count_frequencies(const char * s, int * alphabet, int addend) {
+    char ch;
+    while ((ch = *s++) != 0) {
+        if (islower(ch)) {
+            alphabet[ch - 'a'] += addend;
+        }
+    }
+}
+
 
 /*check if one string is an anagram of another, it uses an int array
  * called alphabet to store frequencies of chars in both strings, add
  * 1 for s1 and subtract 1 for s2*/
 int are_anagrams(const char *s1, const char *s2) {
     int alphabet[26] = { 0 };
-    int index1, index2;
     size_t l1 = strlen(s1), l2 = strlen(s2), i;
 
     /*if the strings have different lengths are not anagrams */
     if (l1 != l2) return 0;
 
     /* count the frequencies of characters */
-    for (i = 0; i < l1; ++i) {
-        index1 = s1[i] - 'a';
-        index2 = s2[i] - 'a';
-        ++alphabet[index1];
-        --alphabet[index2];
-    }
+    count_frequencies(s1, alphabet, 1);
+    count_frequencies(s2, alphabet, -1);
+
     /* all the alphabet letters should be 0, otherwise the strings are not
      * anagrams */
     for (i = 0; i < 26; ++i)
